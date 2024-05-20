5d6e91ff6626b11e05934efb9f98a6156c485081
nanaantwii <143400442+nanaantwii@users.noreply.github.com>
2024-05-20 18:50:08 +0100

    undo

--- js/script.js
+++ js/script.js
@@ -119,7 +119,6 @@ document.addEventListener('DOMContentLoaded', () => {
         score = 0;
         scoreHeader.textContent = score;
         startButton.classList.add('hide');
-        resultsContainer.classList.add('hide');
         shuffledQuestions = questions.sort(() => Math.random() - .5);
         currentQuestionIndex = 0;
         questionContainerElement.classList.remove('hide');
@@ -201,12 +200,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Function to restart the quiz
     function restartQuiz() {
-        score = 0;
-        scoreHeader.textContent = score;
         resultsContainer.classList.add('hide');
         startButton.classList.remove('hide');
+        scoreHeader.textContent = 0;
+        score = 0;
         resetState();
-        questionContainerElement.classList.add('hide');
-        setNextQuestion();
+        startQuiz();
     }
 });
